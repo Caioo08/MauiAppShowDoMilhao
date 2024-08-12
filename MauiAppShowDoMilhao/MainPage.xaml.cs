09e5d52f743874d7941e79c962dfd6fc1ce0bc55
Usuario <Usuario@CPD03-M16>
2024-08-12 15:55:03 -0300

    .

--- MauiAppShowDoMilhao/MainPage.xaml.cs
+++ MauiAppShowDoMilhao/MainPage.xaml.cs
@@ -1,24 +1,58 @@
-﻿namespace MauiAppShowDoMilhao
+﻿using MauiAppShowDoMilhao.Models;
+
+namespace MauiAppShowDoMilhao
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+
+
+        private void Button_Clicked(object sender, EventArgs e)
         {
-            count++;
+            this.BindingContext = App.getRandomPerguntaFacil();
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
+        private void Button_Clicked_Proxima(object sender, EventArgs e)
+        {
+            string texto_alternativa = "";
+
+            bool resposta_correta = false;
+
+            if (alt_1.IsChecked)
+            {
+                texto_alternativa = alt_1.Content.ToString();
+                resposta_correta = (bool)alt_1.Value;
+            }
+            if (alt_2.IsChecked)
+            {
+                texto_alternativa = alt_2.Content.ToString();
+                resposta_correta = (bool)alt_2.Value;
+            }
+            if (alt_3.IsChecked)
+            {
+                texto_alternativa = alt_3.Content.ToString();
+                resposta_correta = (bool)alt_3.Value;
+            }
+            if (alt_4.IsChecked)
+            {
+                texto_alternativa = alt_4.Content.ToString();
+                resposta_correta = (bool)alt_4.Value;
+            }
+
+            if (resposta_correta)
+            {
+                this.BindingContext = App.getRandomPerguntaFacil();
+
+                DisplayAlert("Acertou", texto_alternativa, "Próxima");
+            }
             else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            {
+                DisplayAlert("Errou!", "burro!", "Tentar Novamente");
+            }
         }
     }
 
