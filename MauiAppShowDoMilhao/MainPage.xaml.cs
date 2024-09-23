d3595c7dd6f0707001e31c3aee73fbd4140a1aae
Usuario <Usuario@CPD03-M16>
2024-09-23 15:44:56 -0300

    .

--- MauiAppShowDoMilhao/MainPage.xaml.cs
+++ MauiAppShowDoMilhao/MainPage.xaml.cs
@@ -7,9 +7,76 @@ namespace MauiAppShowDoMilhao
         public MainPage()
         {
             InitializeComponent();
+
+            this.BindingContext = App.getRandonPerguntaFacil();
+
+            lbl_nivel.text = "Fácil";
+            lbl_premio.text = premio.ToString("C");
+
+            Stream track = FileSystem.OpenPackageFyleAsync("0.mp3").Result;
+            AudioManager.Current.CreatePlayer(track).Play();
         }
 
+        private void toca_som()
+        {
+            string track = "";
 
+            switch (pergunta_count)
+            {
+                case 1:
+                    track = "1.wav";
+                    break;
+                case 2:
+                    track = "2.wav";
+                    break;
+                case 3:
+                    track = "3.wav";
+                    break;
+                case 4:
+                    track = "4.wav";
+                    break;
+                case 5:
+                    track = "5.wav";
+                    break;
+                case 6:
+                    track = "6.wav";
+                    break;
+                case 7:
+                    track = "7.wav";
+                    break;
+                case 8:
+                    track = "8.wav";
+                    break;
+                case 9:
+                    track = "9.wav";
+                    break;
+                case 10:
+                    track = "10.wav";
+                    break;
+                case 11:
+                    track = "11.wav";
+                    break;
+                case 12:
+                    track = "12.wav";
+                    break;
+                case 13:
+                    track = "13.wav";
+                    break;
+                case 14:
+                    track = "14.wav";
+                    break;
+                case 15:
+                    track = "15.wav";
+                    break;
+                case 16:
+                    track = "16.wav";
+                    break;
+                
+            }
+
+            AudioManager.Current.CreatePlayer(
+                FyleSystem.OpenAppPackageFyleAsync(track).Result).Play();
+        }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
@@ -79,4 +146,5 @@ namespace MauiAppShowDoMilhao
             lbl_pergunta_numero.Text = pergunta_count.ToString();
 
         }
-    
+
+
